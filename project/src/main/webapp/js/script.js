70e441a4267e7df5ecff697510846af23eecc6a3
IsabelleLePivain <isabelle.lepivain@centrale-marseille.fr>
2018-11-10 17:03:31 +0100

    JSP-8 Seulement boutons. Ajout de recettes pas encore au point.

--- project/src/main/webapp/js/script.js
+++ project/src/main/webapp/js/script.js
@@ -21,6 +21,15 @@ $(function(){
     $(document).on('click', 'button[data-role="removeIngredient"]', function() {
         $(this).parents('.row')[0].remove();
     });
+    // Boutons d'ajout du sel et du poivre (JSP-8)
+    $('button[data-role="ajouterSel"]').click(function() {
+        var index = $('#ingredients > div').length;
+        $('#ingredients').append($('<div>').load('/admin/recettes/ingredientFormRow?ingredientIndex=' + index + '&ingredientName=Sel&ingredientQuantity=0.5&ingredientUnit=g'));
+    });
+    $('button[data-role="ajouterPoivre"]').click(function() {
+        var index = $('#ingredients > div').length;
+        $('#ingredients').append($('<div>').load('/admin/recettes/ingredientFormRow?ingredientIndex=' + index + '&ingredientName=Poivre&ingredientQuantity=0.3&ingredientUnit=g'));
+    });
 });
 
 
